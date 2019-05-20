62cc3bdcf8f24d593f1e821e81f699d1c7f01343
lukasy09 <lukasy09@gmail.com>
2019-05-20 18:02:24 +0200

    normalized operaratins

---
+++ src/logic/basic-operations/BasicOperationExecutor.js
@@ -0,0 +1,7 @@
+export default class BasicOperationExecutor{
+
+    add(firstRGB, secondRGB){}
+    subtract(){}
+    combineLinear(parameter){}
+    negative(){}
+}
--- src/logic/basic-operations/BasicOperationManager.js
+++ src/logic/basic-operations/BasicOperationManager.js
@@ -33,17 +33,54 @@ export default class BasicOperationManager {
         let modifiedImageData = [];
         for (let i = 0; i < imagesLength; i = i + 4) {
 
-            const modifiedRedChannel = leftImageDataArray[i] + rightImageDataArray[i];
-            const modifiedGreenChannel = leftImageDataArray[i + 1] + rightImageDataArray[i + 1];
-            const modifiedBlueChannel = leftImageDataArray[i + 2] + rightImageDataArray[i + 2];
-            const modifiedAlphaChannel = 255;
-
-            modifiedImageData.push(modifiedRedChannel);
-            modifiedImageData.push(modifiedGreenChannel);
-            modifiedImageData.push(modifiedBlueChannel);
-            modifiedImageData.push(modifiedAlphaChannel);
+            const rgb = {
+                red: leftImageDataArray[i] + rightImageDataArray[i],
+                green:leftImageDataArray[i + 1] + rightImageDataArray[i + 1],
+                blue: leftImageDataArray[i + 2] + rightImageDataArray[i + 2],
+                alpha: leftImageDataArray[i + 3] + rightImageDataArray[i + 3]
+            };
+            this.pushRGBObjectIntoArray(modifiedImageData, rgb);
         }
-        let outputModifiedImageDataArray = new Uint8ClampedArray(modifiedImageData);
+
+        let normalizedModifiedImageData = this.normalizeRGBArray(modifiedImageData);
+        let outputModifiedImageDataArray = new Uint8ClampedArray(normalizedModifiedImageData);
         return new ImageData(outputModifiedImageDataArray, leftImageData.width, leftImageData.height);
     };
+
+
+    /**
+     * Helper method decomposing a RGB object and pushing into an array
+     * @param outputArray
+     * @param rgbObject
+     */
+    pushRGBObjectIntoArray = (outputArray, rgbObject) =>{
+        for(let prop in rgbObject){
+            if(rgbObject.hasOwnProperty(prop)){
+                outputArray.push(rgbObject[prop]);
+            }
+        }
+    };
+
+    findArrayMax=(arr) =>{
+        let max = 0;
+        for(let num of arr){
+            if(num > max){
+                max = num;
+            }
+        }
+        return max;
+    };
+
+    normalizeRGBArray = (arr) =>{
+        const max = this.findArrayMax(arr);
+        const length = arr.length;
+
+        for(let i=0; i < length; i++){
+            arr[i] = (arr[i]/max) * 255;
+        }
+
+        return arr;
+    }
+
+
 }
